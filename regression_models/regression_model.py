b122b72c57379e2745719160aa1ed7cca9aae3ee
Georgii <nigmatulin.ga@phystech.edu>
2021-10-10 16:50:16 +0300

    Added permutation importance in regression_model.py. Added description in house-prices-scheme-experiments.xlsx

--- regression_models/regression_model.py
+++ regression_models/regression_model.py
@@ -1,6 +1,8 @@
 import lightgbm as lgb
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.inspection import permutation_importance
 
 def get_rmse_score(X, y):
     # Initializing model.
@@ -12,4 +14,16 @@ def get_rmse_score(X, y):
     # Cross-validation.
     n_scores = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv=cv, n_jobs=-1, error_score='raise')
 
+
+    if 'anomalie' in X.columns:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+        model.fit(X_train, y_train)
+        r = permutation_importance(model, X_test, y_test, n_repeats=10, random_state=0)
+
+        print(f"\nThe relative importance of the {X.columns[-1]} indicator: {r.importances_mean[-1]:.7f} "
+              f"({r.importances_std[-1]:.7f})")
+    else:
+        pass
+
     return -n_scores.mean(), n_scores.std()
+
